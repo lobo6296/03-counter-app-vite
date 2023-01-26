723c7129ab66de32eb24f1151d30cbc0bd55d1f5
Jose Zamora <azamor4@gmail.com>
2023-01-25 21:19:55 -0600

    video 35

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,17 @@
+// function App() {
+//     return (<h1>Hola World App!!!</h1>);
+// }
+
+// export default App;
+
+// export function App() {
+//     return (<h1>Hola World App!!!</h1>);
+// }
+
+export const HelloWorldApp = () => {
+  return (
+    <div>Hello World App</div>
+  );
+}
+
+//export const HelloWorldApp = () => <h1>Arrow Function</h1>;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
+// import App from './HelloWorldApp'
 
-function App() {
-    return (<h1>Hola Mundo</h1>);
-}
+
+
 
 ReactDOM.createRoot(document.getElementById('root') ).render(
     <React.StrictMode>
-        <App />
+        <HelloWorldApp />
     </React.StrictMode>
 )
 
