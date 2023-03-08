964c729c5f3e087fba5f1634b0e2e0f90ada9d04
José Antonio Zamora Gómez <azamor4@gmail.com>
2023-03-07 21:46:18 -0600

    cambios 7-3-2023
    counter app

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
-import { HelloWorldApp } from './HelloWorldApp';
+import { CounterApp } from './CounterApp';
+//import { FirstApp } from './FirstApp';
+//import { HelloWorldApp } from './HelloWorldApp';
 // import App from './HelloWorldApp'
 import './styles.css';
 
 
 
-ReactDOM.createRoot(document.getElementById('root') ).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title="el titulo" subTitle={123}/>
+        <CounterApp value={10} />
     </React.StrictMode>
 )
 
